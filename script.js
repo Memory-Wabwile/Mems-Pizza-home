5f7713f5166b66c0107383483d0897b510fa21f6
Memory-Wabwile <memorywabwile@gmail.com>
2022-01-17 17:18:46 +0300

    validate the form on js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
         let deliverr = document.getElementById("deliver");
 
         if(deliverr.checked==true){
+            let delivery = 150;
            let location = prompt("Where would you like your pizza delivered?");
            alert ("You will have your pizza delivered at ${location}");
         }
@@ -10,3 +11,64 @@ $(document).ready(function(){
     }
 });
 
+let sizee = ""
+if(small.selected){
+    sizee = "small";
+}
+else if (medium.selected){
+    sizee = "medium"
+}
+else if (localStorage.selected){
+    sizee = "large"
+};
+
+if(sizee=="small"){
+    $("#size").append("small")
+}
+else if (sizee=="medium"){
+    $("#size").append("medium")
+}
+else if (sizee=="large"){
+    $("#size").append("large")
+}
+
+let crust=""
+if (Crispy.selected){
+    crust="Crispy";
+}
+else if(Stuffed.selected){
+    crust="Stuffed";
+}
+else if (Gluten-free.selected){
+    crust="Gluten-free";
+}
+
+if(crust=="Crispy"){
+    $("#crust").append("crispy")
+}
+else if(crust=="Stuffed"){
+    $("#crust").append("stuffed")
+}
+else if(crust=="Gluten-free"){
+    $("#crust").append("Gluten-free")
+}
+
+let toppings=""
+if (sausage.checked){
+    toppings="sausage"
+}
+else if (mushroom.checked){
+    toppings="mushroom"
+}
+else if (gorgonzola.checked){
+    toppings="Gorgonzola"
+}
+if(toppings=="sausage"){
+    $("#toppings").append("sausage")
+}
+else if(toppings=="mushroom"){
+    $("#toppings").append("mushroom")
+}
+else if(toppings=="gorgonzola"){
+    $("#toppings").append("gorgonzola")
+}
