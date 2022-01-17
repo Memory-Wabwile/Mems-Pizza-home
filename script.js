b03b294a2c63575e447c3e7e57258a5afd651df3
Memory-Wabwile <memorywabwile@gmail.com>
2022-01-17 13:58:06 +0300

    add the prompt message for delivery

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    function fcn(){
+        let deliverr = document.getElementById("deliver");
+
+        if(deliverr.checked==true){
+           let location = prompt("Where would you like your pizza delivered?");
+           alert ("You will have your pizza delivered at ${location}");
+        }
+         break 
+    }
+});
+
