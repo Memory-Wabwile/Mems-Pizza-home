b90fe3e859ac3d95fee1f7f76f1bfac41dc55bab
Memory-Wabwile <memorywabwile@gmail.com>
2022-01-30 17:52:55 +0300

    specify the  prices of each item

--- script.js
+++ script.js
@@ -1,64 +1,124 @@
-function fcn(){
-   
-    let deliverr = document.getElementById("deliver");
-    
-    if(deliverr.checked==false){
-        let delivery = 150;
+// function fcn() {
+//   let deliverr = document.getElementById("deliver");
 
-       let location = prompt("Where would you like your pizza delivered?");
-       alert (`You will have your pizza delivered at ${location}`);
-    }
-     
-}
+//   if (deliverr.checked == false) {
+//     let delivery = 150;
+
+//     let location = prompt("Where would you like your pizza delivered?");
+//     alert(`You will have your pizza delivered at ${location}`);
+//   }
+// }
 // $(document).ready(function(){
-  
+
 // });
 
+// const order = document.querySelector("#button");
+// order.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   console.log("hello world");
+//   const size = document.querySelector("#size");
 
-  const order = document.querySelector("#button");
-  order.addEventListener("click",function(e){
-      e.preventDefault()
-      console.log("hello world")
-    const size = document.querySelector("#size");
+//   let sizee = "";
+//   if (size.value == "small") {
+//     sizee = "small";
+//     console.log(sizee);
+//   } else if (size.value == "medium") {
+//     sizee = "medium";
+//     console.log(sizee);
+//   } else if (size.value == "large") {
+//     sizee = "large";
+//     console.log(sizee);
+//   }
 
+//   const crusts = document.querySelector("#crust");
+//   let crust = "";
+//   if (crusts.value == "crispy") {
+//     crust = "crispy";
+//     console.log(crust);
+//   } else if (crusts.value == "stuffed") {
+//     crust = "stuffed";
+//     console.log(crust);
+//   } else if (crusts.value == "gluten-free") {
+//     crust = "gluten-free";
+//     console.log(crust);
+//   }
 
-let sizee = "";
-  if (size.value == "small") {
-    sizee = "small";
-    console.log(sizee);
-  } else if (size.value == "medium") {
-    sizee = "medium";
-    console.log(sizee);
-  } else if (size.value == "large") {
-    sizee = "large";
-    console.log(sizee);
+//   const topping = document.querySelector("#toppings");
+
+//   let toppings = "";
+//   if (topping.value == "sausage") {
+//     toppings = "sausage";
+//     console.log(topping);
+//   } else if (topping.value == "mushroom") {
+//     toppings = "mushroom";
+//     console.log(topping);
+//   } else if (topping.value == "orgonzola") {
+//     toppings = "orgonzola";
+//     console.log(topping);
+//   }
+// });
+
+class pizzaType{
+  constructor(pizza){
+    this.pizza=pizza
   }
+}
 
-const crusts = document.querySelector("#crust");
-let crust = "";
-  if (crusts.value == "crispy") {
-    crust = "crispy";
-    console.log(crust);
-  } else if (crusts.value == "stuffed") {
-    crust = "stuffed";
-    console.log(crust);
-  } else if (crusts.value == "gluten-free") {
-    crust = "gluten-free";
-    console.log(crust);
+// let pizzaType = {
+//   pepperoni: "pepperoni",
+//   boerewors: "boerewors",
+//   veggie: "veggieTikka"
+// };
+
+let pizzaPrice = {
+  small:800,
+  medium:1000,
+  large:1200
+}
+
+let crustPrice={
+  crispy:100,
+  stuffed:150,
+  gluttenFree:200
+}
+
+class crust{
+  constructor(crust){
+    this.crustName = crust,
+    this.crustPrice = crustPrice[crust]
   }
-  
-const topping = document.querySelector("#toppings");
+}
 
+let toppingsPrice={
+  sausage:50,
+  mushroom:50,
+  gorgonzola:50
+}
 
-  let toppings = "";
-  if (topping.value == "sausage") {
-    toppings = "sausage";
-    console.log(topping);
-  } else if (topping.value == "mushroom") {
-    toppings = "mushroom";
-    console.log(topping);
-  } else if (topping.value == "orgonzola") {
-    toppings = "orgonzola";
-    console.log(topping);
+class toppings{
+  constructor(tops){
+    this.toppingsName=tops,
+    this.toppingsPrice=toppingsPrice[tops]
   }
-  })
+}
+
+class order{
+  constructor(pizzaType , pizzaSize , toppings , crust , number){
+    this.pizzaType=pizzaType,
+    this.pizzaSize=pizzaSize,
+    this.pizzaPrice=pizzaPrice[pizzaSize],
+    this.crust=crust,
+    this.toppings=toppings,
+    this.number=number
+  }
+}
+
+let form = document.querySelector("form")
+
+form.addEventListener("submit" , function(){
+let pizza=document.querySelector("#type")
+let size =document.querySelector("#size")
+let crustt =document.querySelector("#crust")
+let topppings =document.querySelectorAll("#sausage")
+
+})
